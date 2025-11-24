8afbe7420f8eee87ff3a091068f1630312d125ca
Philip Oliveira <mathphilips007@gmail.com>
2025-11-24 20:22:13 -0300

    feat: Initial implementation of Java Quadratic Formula Calculator

---
+++ QuadraticFormulaCalculator.java
@@ -0,0 +1,31 @@
+package QuadraticFormulaCalculator;
+
+import java.text.DecimalFormat;
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.lang.Math;
+
+public class QuadraticFormulaCalculator {
+    static void main() {
+        Scanner sc = new Scanner(System.in);
+        ArrayList<Integer> variables = new ArrayList<>(2);
+        DecimalFormat d1 = new DecimalFormat("#.##");
+        for (int i = 0; i < 3; i++){
+            variables.add(sc.nextInt());
+        }
+        System.out.println("a: " + variables.get(0) + ", b: " + variables.get(1) + ", c: " + variables.get(2));
+
+        int square = (int) Math.pow(variables.get(1), 2);
+        int four = 4 * variables.get(0) * variables.get(2);
+        int squareRootNum = square - four;
+        float squareRoot = (float) Math.pow(squareRootNum, 0.5);
+        int b = -variables.get(1);
+        float positiveAbove = b + squareRoot;
+        float negativeAbove = b - squareRoot;
+        float firstSolution = positiveAbove / (2 * variables.get(0));
+        float secondSolution = negativeAbove / (2 * variables.get(0));
+        System.out.println("There's your results: the first solution is: " + d1.format(firstSolution) + " and the second solution is: " + d1.format(secondSolution));
+        sc.close();
+        System.exit(0);
+    }
+}
